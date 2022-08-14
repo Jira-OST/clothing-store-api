a255d7451f884e464d97cad96523c408325f33a2
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-08-14 19:13:00 +0200

    Initial commit

---
+++ src/main/java/com/example/clothingstoreapi/ClothingStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothingstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clothingstoreapi/ClothingStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothingstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
