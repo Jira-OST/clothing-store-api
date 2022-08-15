b286ed5442f31700eeea78c1fe18c999b5b2a479
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-08-15 14:17:22 +0200

    edit secret key value to be an env variable

--- src/main/java/com/example/clothingstoreapi/Security/Util/JwtUtil.java
+++ src/main/java/com/example/clothingstoreapi/Security/Util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.example.clothingstoreapi.Security.Util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,8 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String secret = "javatechie";
+    @Value("${app.jwt.secret}")
+    private String secret;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
