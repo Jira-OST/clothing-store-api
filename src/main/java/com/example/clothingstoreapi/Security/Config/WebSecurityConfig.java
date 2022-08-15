e790333fa9911f39c0b7d628e8cc4240afef176d
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-08-15 13:52:03 +0200

    Config Spring data and jwtToken Utility

--- src/main/java/com/example/clothingstoreapi/Security/Config/WebSecurityConfig.java
+++ src/main/java/com/example/clothingstoreapi/Security/Config/WebSecurityConfig.java
@@ -1,2 +1,53 @@
-package com.example.clothingstoreapi.Security.Config;public class WebSecurityConfig {
+package com.example.clothingstoreapi.Security.Config;
+
+import com.example.clothingstoreapi.Security.Filters.JwtFilter;
+import com.example.clothingstoreapi.Security.Service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/api/auth/register", "/api/auth/login", "/api/users")
+                .permitAll().anyRequest().authenticated()
+                .and().exceptionHandling().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+    }
 }
--- src/main/java/com/example/clothingstoreapi/Security/Service/CustomUserDetailsService.java
+++ src/main/java/com/example/clothingstoreapi/Security/Service/CustomUserDetailsService.java
@@ -1,2 +1,26 @@
-package com.example.clothingstoreapi.Security.Service;public class CustomUserDetailsService {
+package com.example.clothingstoreapi.Security.Service;
+
+import com.example.clothingstoreapi.Persistence.Entities.UserEntity;
+import com.example.clothingstoreapi.Persistence.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepo.findByEmail(email).get();
+        if (user == null) {
+
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
 }
