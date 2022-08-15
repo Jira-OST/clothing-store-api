744479e76e645e94edc0d1d7fff34fee88da2c24
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-08-15 14:06:31 +0200

    adding the following dependencies

--- src/main/java/com/example/clothingstoreapi/ClothingStoreApiApplication.java
+++ src/main/java/com/example/clothingstoreapi/ClothingStoreApiApplication.java
@@ -1,7 +1,11 @@
 package com.example.clothingstoreapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ClothingStoreApiApplication {
@@ -9,5 +13,14 @@ public class ClothingStoreApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClothingStoreApiApplication.class, args);
     }
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        return mapper;
+
+    }
+
 
 }
