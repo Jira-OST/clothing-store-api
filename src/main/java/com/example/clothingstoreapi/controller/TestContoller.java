22f9a3ed1485bd07f294fe236d2ebf2e583fc331
MoMagdy <14momagdi@gmail.com>
2022-08-16 22:52:41 +0200

    Modify: Add Cross Origin annotation to all controllers

--- src/main/java/com/example/clothingstoreapi/controller/TestContoller.java
+++ src/main/java/com/example/clothingstoreapi/controller/TestContoller.java
@@ -3,10 +3,12 @@ package com.example.clothingstoreapi.controller;
 import com.example.clothingstoreapi.security.Util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class TestContoller {
 
     @Autowired
