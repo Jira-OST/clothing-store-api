79ec3e3b46ae8b65507f996e24e0c4dcdcf0deea
MoMagdy <14momagdi@gmail.com>
2022-08-17 12:10:24 +0200

    Exclude all product from authentication

--- src/main/java/com/example/clothingstoreapi/security/Config/WebSecurityConfig.java
+++ src/main/java/com/example/clothingstoreapi/security/Config/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/api/auth/register", "/api/auth/login", "/api/users", "/v3/api-docs/**", "/swagger-ui/**" )
+                .antMatchers("/api/auth/register", "/api/auth/login", "/api/users", "/v3/api-docs/**", "/swagger-ui/**" ,"/product/v1/all")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
